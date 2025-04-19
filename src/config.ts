f851e9f142a0cc360550d76b5c9ece711181e988
Vince <31858345+vsusini@users.noreply.github.com>
2025-04-19 19:04:00 -0400

    Decrease threshold to only show when 1 minute is left

--- src/config.ts
+++ src/config.ts
@@ -4,5 +4,5 @@ export const config = {
   DEFAULT_GUILD_ID: "952044361334550548",
   REWARDS_CHECK_INTERVAL: 60 * 1, // 1 minute in seconds
   REWARDS_NOTIFICATION_CHANNEL: "1087064876930838590",
-  REWARDS_NOTIFICATION_THRESHOLD: 60 * 60 * 6, // Notify when less than 60 seconds remaining
+  REWARDS_NOTIFICATION_THRESHOLD: 60, // Notify when less than 60 seconds remaining
 } as const;
